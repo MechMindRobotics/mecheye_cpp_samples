44a5ec57ee7158e4c7321fdc79fffdfaf25462b7
Lianchangle <lsiricreat@163.com>
2023-11-29 11:44:20 +0800

    samples: update mecheye_cpp_samples to SDK 2.2.0

---
+++ profiler/AcquireProfileDataUsingCallback/AcquireProfileDataUsingCallback.cpp
@@ -0,0 +1,265 @@
+﻿/*******************************************************************************
+ *BSD 3-Clause License
+ *
+ *Copyright (c) 2016-2023, Mech-Mind Robotics
+ *All rights reserved.
+ *
+ *Redistribution and use in source and binary forms, with or without
+ *modification, are permitted provided that the following conditions are met:
+ *
+ *1. Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ *2. Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ *3. Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ *THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ *FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ *OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ******************************************************************************/
+
+/*
+With this sample, you can acquire the profile data using a callback function, generate the intensity
+image and depth map, and save the images.
+*/
+
+#include <opencv2/imgcodecs.hpp>
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <mutex>
+#include "profiler/Profiler.h"
+#include "profiler/api_util.h"
+#include "profiler/parameters/ProfileProcessingParameters.h"
+#include "profiler/parameters/ProfileExtractionParameters.h"
+#include "profiler/parameters/RawImageParameters.h"
+#include "profiler/parameters/ScanParameters.h"
+
+namespace {
+std::mutex kMutex;
+
+// Define the callback function for retrieving the profile data
+void callbackFunc(const mmind::eye::ProfileBatch& batch, void* pUser)
+{
+    std::unique_lock<std::mutex> lock(kMutex);
+    auto* outPutBatch = static_cast<mmind::eye::ProfileBatch*>(pUser);
+    outPutBatch->append(batch);
+}
+
+void saveMap(const mmind::eye::ProfileBatch& batch, int lineCount, int width,
+             const std::string& path)
+{
+    if (batch.isEmpty()) {
+        std::cout << "The batch of profiles is empty." << std::endl;
+        return;
+    }
+    cv::imwrite(path, cv::Mat(lineCount, width, CV_32FC1, batch.getDepthMap().data()));
+}
+
+void saveIntensity(const mmind::eye::ProfileBatch& batch, int lineCount, int width,
+                   const std::string& path)
+{
+    if (batch.isEmpty()) {
+        std::cout << "The batch of profiles is empty." << std::endl;
+        return;
+    }
+    cv::imwrite(path, cv::Mat(lineCount, width, CV_8UC1, batch.getIntensityImage().data()));
+}
+} // namespace
+
+int main()
+{
+    mmind::eye::Profiler profiler;
+    if (!findAndConnect(profiler))
+        return -1;
+
+    if (!confirmCapture()) {
+        profiler.disconnect();
+        return -1;
+    }
+
+    mmind::eye::UserSet currentUserSet = profiler.currentUserSet();
+    // Set the "Exposure Mode" parameter to "Timed"
+    showError(currentUserSet.setEnumValue(
+        mmind::eye::brightness_settings::ExposureMode::name,
+        static_cast<int>(mmind::eye::brightness_settings::ExposureMode::Value::Timed)));
+
+    // Set the "Exposure Time" parameter to 100 μs
+    showError(currentUserSet.setIntValue(mmind::eye::brightness_settings::ExposureTime::name, 100));
+
+    /*You can also use the HDR exposure mode, in which the laser profiler exposes in three phases
+    while acquiring one profile. In this mode, you need to set the total exsposure time, the
+    proportions of the three exposure phases, as well as the two thresholds of grayscale values. The
+    code for setting the relevant parameters for the HDR exposure mode is given in the following
+    comments.*/
+
+    // Set the "Exposure Mode" parameter to "HDR"
+    // showError(currentUserSet.setEnumValue(
+    //     mmind::eye::brightness_settings::ExposureMode::name,
+    //     static_cast<int>(mmind::eye::brightness_settings::ExposureMode::Value::HDR)));
+
+    // Set the total exposure time to 100 μs
+    // showError(currentUserSet.setIntValue(mmind::eye::brightness_settings::ExposureTime::name,
+    // 100));
+
+    // Set the proportion of the first exposure phase to 40%
+    // showError(currentUserSet.setFloatValue(
+    //     mmind::eye::brightness_settings::HdrExposureTimeProportion1::name, 40));
+
+    // Set the proportion of the first + second exposure phases to 80% (that is, the second exposure
+    // phase occupies 40%, and the third exposure phase occupies 20% of the total exposure time)
+    // showError(currentUserSet.setFloatValue(
+    //     mmind::eye::brightness_settings::HdrExposureTimeProportion2::name, 80));
+
+    // Set the first threshold to 10. This limits the maximum grayscale value to 10 after the first
+    // exposure phase is completed. showError(
+    //     currentUserSet.setFloatValue(mmind::eye::brightness_settings::HdrFirstThreshold::name,
+    //     10));
+
+    // Set the second threshold to 60. This limits the maximum grayscale value to 60 after the
+    // second exposure phase is completed. showError(currentUserSet.setFloatValue(
+    //     mmind::eye::brightness_settings::HdrSecondThreshold::name, 60));
+
+    // Set the "Data Acquisition Trigger Source" parameter to "Software"
+    showError(currentUserSet.setEnumValue(
+        mmind::eye::trigger_settings::DataAcquisitionTriggerSource::name,
+        static_cast<int>(
+            mmind::eye::trigger_settings::DataAcquisitionTriggerSource::Value::Software)));
+
+    // Set the "Data Acquisition Trigger Source" parameter to "External"
+    // showError(currentUserSet.setEnumValue(
+    //     mmind::eye::trigger_settings::DataAcquisitionTriggerSource::name,
+    //     static_cast<int>(mmind::eye::trigger_settings::DataAcquisitionTriggerSource::Value::External)));
+
+    // Set the "Line Scan Trigger Source" parameter to "Fixed rate"
+    showError(currentUserSet.setEnumValue(
+        mmind::eye::trigger_settings::LineScanTriggerSource::name,
+        static_cast<int>(mmind::eye::trigger_settings::LineScanTriggerSource::Value::FixedRate)));
+    // Set the "Software Trigger Rate" to 1000 Hz
+    showError(currentUserSet.setFloatValue(mmind::eye::trigger_settings::SoftwareTriggerRate::name,
+                                           1000));
+    // Set the "Scan Line Count" parameter (the number of lines to be scanned) to 1600
+    showError(currentUserSet.setIntValue(mmind::eye::scan_settings::ScanLineCount::name, 1600));
+    // Set the timeout for retrieving profile data by callback to 4000 ms. The timeout means that
+    // the maximum time to wait for retrieving profile data fully, so that it needs to be set
+    // according to the trigger rate. If the timeout occurs and profile data are not yet fully
+    // retrieved, the missing profiles will be filled with invalid data.
+    showError(currentUserSet.setIntValue(mmind::eye::scan_settings::CallbackRetrievalTimeout::name,
+                                         4000));
+    // Set the "Laser Power" parameter to 100
+    showError(currentUserSet.setIntValue(mmind::eye::brightness_settings::LaserPower::name, 100));
+
+    mmind::eye::ProfilerInfo profilerInfo;
+    if (profiler.getProfilerInfo(profilerInfo).isOK()) {
+        if (profilerInfo.model == "Mech-Eye LNX 8030") {
+            // Set the "Analog Gain" parameter for LNX-8030 to "1.3×"
+            showError(currentUserSet.setEnumValue(
+                mmind::eye::brightness_settings::AnalogGainFor8030::name,
+                static_cast<int>(
+                    mmind::eye::brightness_settings::AnalogGainFor8030::Value::Gain_1_3)));
+        } else {
+            // Set the "Analog Gain" parameter for other models to "1.3×"
+            showError(currentUserSet.setEnumValue(
+                mmind::eye::brightness_settings::AnalogGain::name,
+                static_cast<int>(mmind::eye::brightness_settings::AnalogGain::Value::Gain_1_3)));
+        }
+    }
+
+    // Set the "Digital Gain" parameter to 0
+    showError(currentUserSet.setIntValue(mmind::eye::brightness_settings::DigitalGain::name, 0));
+    // Set the "Minimum Grayscale Value" parameter to 50
+    showError(
+        currentUserSet.setIntValue(mmind::eye::profile_extraction::MinGrayscaleValue::name, 50));
+    // Set the "Minimum Laser Line Width" parameter to 2
+    showError(
+        currentUserSet.setIntValue(mmind::eye::profile_extraction::MinLaserLineWidth::name, 2));
+    // Set the "Maximum Laser Line Width" parameter to 20
+    showError(
+        currentUserSet.setIntValue(mmind::eye::profile_extraction::MaxLaserLineWidth::name, 20));
+    // Set the "Minimum Spot Intensity" parameter to 51
+    showError(
+        currentUserSet.setIntValue(mmind::eye::profile_extraction::MinSpotIntensity::name, 51));
+    // Set the "Maximum Spot Intensity" parameter to 205
+    showError(
+        currentUserSet.setIntValue(mmind::eye::profile_extraction::MaxSpotIntensity::name, 205));
+    /* Set the "Gap Filling" parameter to 16, which controls the size of the gaps that can be filled
+     * in the profile. When the number of consecutive data points in a gap in the profile is no
+     * greater than 16, this gap will be filled. */
+    showError(currentUserSet.setIntValue(mmind::eye::profile_processing::GapFilling::name, 16));
+    // Set the "Spot Selection" parameter to "Strongest"
+    showError(currentUserSet.setEnumValue(
+        mmind::eye::profile_extraction::SpotSelection::name,
+        static_cast<int>(mmind::eye::profile_extraction::SpotSelection::Value::Strongest)));
+
+    /* Set the "Filter" parameter to "Mean". The "Mean Filter Window Size" parameter needs to be set
+     * as well. This parameter controls the window size of mean filter. If the "Filter" parameter is
+     * set to "Median", the "Median Filter Window Size" parameter needs to be set. This parameter
+     * controls the window size of median filter.*/
+    showError(currentUserSet.setEnumValue(
+        mmind::eye::profile_processing::Filter::name,
+        static_cast<int>(mmind::eye::profile_processing::Filter::Value::Mean)));
+    // Set the "Mean Filter Window Size" parameter to 2
+    showError(currentUserSet.setEnumValue(
+        mmind::eye::profile_processing::MeanFilterWindowSize::name,
+        static_cast<int>(
+            mmind::eye::profile_processing::MeanFilterWindowSize::Value::WindowSize_2)));
+
+    int dataPoints = 0;
+    // Get the number of data points of each profile
+    showError(currentUserSet.getIntValue(mmind::eye::scan_settings::DataPointsPerProfile::name,
+                                         dataPoints));
+    int captureLineCount = 0;
+    // Get the current value of the "Scan Line Count" parameter
+    currentUserSet.getIntValue(mmind::eye::scan_settings::ScanLineCount::name, captureLineCount);
+
+    // Define a ProfileBatch object to store the profile data
+    mmind::eye::ProfileBatch profileBatch(dataPoints);
+    // Register the callback function
+    auto status = profiler.registerAcquisitionCallback(callbackFunc, &profileBatch);
+    if (!status.isOK()) {
+        showError(status);
+        return -1;
+    }
+
+    // Call the startAcquisition to take the laser profiler into the acquisition ready status
+    status = profiler.startAcquisition();
+    if (!status.isOK()) {
+        showError(status);
+        return -1;
+    }
+
+    // Call the triggerSoftware() to start the data acquisition
+    status = profiler.triggerSoftware();
+    if (!status.isOK()) {
+        showError(status);
+        return -1;
+    }
+
+    while (true) {
+        std::unique_lock<std::mutex> lock(kMutex);
+        if (profileBatch.isEmpty()) {
+            lock.unlock();
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        } else
+            break;
+    }
+
+    saveMap(profileBatch, profileBatch.height(), profileBatch.width(), "DepthMap.tif");
+    profileBatch.clear();
+
+    // Disconnect from the laser profiler
+    profiler.disconnect();
+    return 0;
+}
---
+++ profiler/README.md
@@ -0,0 +1,151 @@
+# C++ Samples for Mech-Eye 3D Laser Profiler
+
+This documentation provides descriptions of Mech-Eye API C++ samples for Mech-Eye 3D Laser Profiler and instructions for building all the samples at once.
+
+If you have any questions or have anything to share, feel free to post on the [Mech-Mind Online Community](https://community.mech-mind.com/). The community also contains a [specific category for development with Mech-Eye SDK](https://community.mech-mind.com/c/mech-eye-sdk-development/19).
+
+## Sample List
+
+Currently, the following samples are provided.
+
+The samples marked with `(OpenCV)` require [OpenCV](https://opencv.org/releases/) to be installed.  
+
+* [AcquireProfileData](https://github.com/MechMindRobotics/mecheye_cpp_samples/tree/master/profiler/AcquireProfileData) `(OpenCV)`  
+  Acquire the profile data, generate the intensity image and depth map, and save the images.
+* [AcquireProfileDataUsingCallback](https://github.com/MechMindRobotics/mecheye_cpp_samples/tree/master/profiler/AcquireProfileData)  `(OpenCV)`  
+  Acquire the profile data using a callback function, generate the intensity image and depth map, and save the images.
+* [AcquirePointCloud](https://github.com/MechMindRobotics/mecheye_cpp_samples/tree/master/profiler/AcquireProfileData)  
+  Acquire the profile data, generate the point cloud, and save the point cloud in the CSV and PLY formats.
+* [ManageUserSets](https://github.com/MechMindRobotics/mecheye_cpp_samples/tree/master/profiler/AcquireProfileData)  
+  Manage user sets, such as obtaining the names of all user sets, adding a user set, switching the user set, and saving parameter settings to the user set.
+* [RegisterProfilerEvent](https://github.com/MechMindRobotics/mecheye_cpp_samples/tree/master/profiler/RegisterProfilerEvent)  
+Define and register the callback function for monitoring the laser profiler connection status.
+
+## Build the Samples
+
+The instructions provided here allow you to build all the samples at once.
+
+### Windows
+
+#### Prerequisites
+
+Please download and install the required software listed below.
+
+* [Mech-Eye SDK (latest version)](https://downloads.mech-mind.com/?tab=tab-sdk)
+* [Visual Studio (version 2017 or above)](https://visualstudio.microsoft.com/vs/community/)
+* [CMake (version 3.2 or above)](https://cmake.org/download/)
+
+Optional software: If you need to build the samples dependent on third-party software (refer to the Sample List above), please install the corresponding software.
+
+* [OpenCV (version 3.4.5 or above)](https://opencv.org/releases/)
+
+#### Instructions
+
+1. Make sure that the samples are stored in a location with read and write permissions.
+2. If OpenCV is installed, add the following directories to the **Path** environment variable:
+
+   * `xxx/opencv/build/x64/vc14/bin`
+   * `xxx/opencv/build/x64/vc14/lib`
+
+3. (Optional) Disable unneeded samples: if OpenCV is installed, this step must be performed.
+
+   Open the CMakeLists file in `xxx/profiler`, and change **ON** to **OFF** in the line that starts with `option(USE_OPENCV`.
+
+4. Run Cmake and set the source and build paths: 
+
+   | Field                       | Path                 |
+   | :----                       | :----                |
+   | Where is the source code    | xxx/profiler         |
+   | Where to build the binaries | xxx/profiler/build   |
+
+5. Click the **Configure** button. In the pop-up window, set the generator and platform according to the actual situation, and then click the **Finish** button.
+6. When the log displays **Configuring done**, click the **Generate** button. When the log displays **Generating done**, click the **Open Project** button.
+7. In Visual Studio toolbar, change the solution configuration from **Debug** to **Release**.
+8. In the menu bar, select **Build** > **Build Solution**. An EXE format executable file is generated for each sample. The executable files are saved to the `Release` folder, located in the **Where to build the binaries** directory.
+9. In the **Solution Explorer** panel, right-click a sample, and select **Set as Startup Project**.
+10. Click the **Local Windows Debugger** button in the toolbar to run the sample.
+11. Enter the index of the camera to which you want to connect, and press the Enter key. The obtained files are saved to the `Release` folder.
+
+### Ubuntu
+
+Ubuntu 18 or above is required.
+
+#### Prerequisites
+
+* Update the software source list.
+
+  ```bash
+  sudo apt-get update
+  ```
+
+* Install required tools.
+  
+  ```bash
+  sudo apt-get install -y build-essential pkg-config cmake
+  ```
+
+* Install [Mech-Eye SDK (latest version)](https://downloads.mech-mind.com/?tab=tab-sdk).
+
+  >Note: If you have installed Mech-Eye SDK before, please uninstall it first with the following command:
+  >
+  >```bash
+  >sudo dpkg -P MechEyeApi
+  >```
+  
+  * If the system architecture is AMD64, execute the following command:
+
+    ```bash
+    sudo dpkg -i 'MechEyeApi_x.x.x_amd64.deb'
+    ```
+
+  * If the system architecture is ARM64, execute the following command:
+
+    ```bash
+    sudo dpkg -i 'MechEyeApi_x.x.x_arm64.deb'
+    ```
+
+* Install optional third-party libraries: If you need to build the samples dependent on third-party software (refer to the Sample List above), please install the corresponding software.
+
+  * Install OpenCV (latest version):
+
+    ```bash
+    sudo apt update && sudo apt install -y unzip
+    wget -O opencv.zip https://github.com/opencv/opencv/archive/4.x.zip
+    unzip opencv.zip
+    mkdir build && cd build
+    cmake ../opencv-4.x
+    cmake --build .
+    sudo make install
+    ```
+
+#### Instructions
+
+1. Navigate to the directory of the sample.
+
+   ```bash
+   cd xxx/profiler/
+   ```
+
+2. (Optional) Disable unneeded samples: if any of the optional software is not installed, this step must be performed.
+
+   Open the CMakeLists file in `xxx/profiler/`, and change **ON** to **OFF** in the options of the unneeded samples.
+
+3. Configure and build the samples.
+
+   ```bash
+   sudo mkdir build && cd build
+   sudo cmake ..
+   sudo make
+   ```
+
+4. Run a sample. Replace `SampleName` with the name of the sample that you want to run.
+
+   ```bash
+   sudo ./SampleName
+   ```
+
+5. Enter the index of the camera to which you want to connect, and press the Enter key. The obtained files are saved to `xxx/profiler/build`.
+
+## License
+
+Mech-Eye Samples are distributed under the [BSD license](https://github.com/MechMindRobotics/mecheye_cpp_samples/blob/master/LICENSE).
